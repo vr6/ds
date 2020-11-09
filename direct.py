3a91bc484561c36e81f97b557dbc1fc28969915d
vr6 <venkat@apache.org>
2020-11-09 08:13:20 +0000

    update

--- direct.py
+++ direct.py
@@ -6,13 +6,17 @@ from sklearn.linear_model import LinearRegression
 x1 = np.random.randn(100)
 x2 = np.random.randn(100)
 er = np.random.randn(100)
-
-y = 2 + 5 * x1 + 7 * x2 + er
 X = np.vstack([np.ones(100), x1, x2]).T
 
-B = np.linalg.inv(X.T @ X) @ X.T @ y
+# y = 2 + 5 * x1 + 7 * x2 + er
+w = np.array([2, 5, 7]).T
+y = X @ w + er
+
+B = np.linalg.inv (X.T @ X) @ X.T @ y
 print (B)
 
 model = LinearRegression()
 model.fit(X, y)
 print(model.intercept_, model.coef_)
+
+# %%
